4c8d7c71040016844efe89bd185e0e7b833c35c2
Roman <levinzon.roman@gmail.com>
2019-12-14 21:20:40 +0100

    Dialog ui update

--- presentation/src/main/java/cz/levinzonr/spoton/presentation/screens/main/MainActivity.kt
+++ presentation/src/main/java/cz/levinzonr/spoton/presentation/screens/main/MainActivity.kt
@@ -11,6 +11,8 @@ import cz.levinzonr.spoton.presentation.extensions.observeNonNull
 import cz.levinzonr.spoton.presentation.views.AppDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
+import android.net.Uri
+
 
 class MainActivity : BaseActivity() {
 
@@ -30,8 +32,25 @@ class MainActivity : BaseActivity() {
 
     private fun showUpdateDialog() {
         AppDialog.Builder(this)
-                .setMessage("Update Available")
+                .setTitle(getString(R.string.update_title))
+                .setMessage(getString(R.string.update_message))
+                .setPositiveButton(getString(R.string.update_now_button)) {
+                    openPlayStore()
+                }
+                .setNegativeButton(getString(R.string.update_later_button)) {
+                    it.dismiss()
+                }
+                .setCancelable(false)
                 .show()
+    }
+
+    private fun openPlayStore() {
+        val appPackageName = packageName // getPackageName() from Context or Activity object
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
+        } catch (anfe: android.content.ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
+        }
     }
 
     companion object {
