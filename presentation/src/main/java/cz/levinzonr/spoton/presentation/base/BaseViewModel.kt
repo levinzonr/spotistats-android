8dd2196266e015b9d6b7b4fd3538e77936384889
Roman <levinzon.roman@gmail.com>
2020-03-02 10:44:40 +0100

    Updated roxie lib

--- presentation/src/main/java/cz/levinzonr/spoton/presentation/base/BaseViewModel.kt
+++ presentation/src/main/java/cz/levinzonr/spoton/presentation/base/BaseViewModel.kt
@@ -2,14 +2,11 @@ package cz.levinzonr.spoton.presentation.base
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import cz.levinzonr.roxie.BaseAction
-import cz.levinzonr.roxie.BaseChange
-import cz.levinzonr.roxie.BaseState
-import cz.levinzonr.roxie.BaseViewModel
+import cz.levinzonr.roxie.*
 import cz.levinzonr.spoton.presentation.navigation.Route
 import cz.levinzonr.spoton.presentation.util.SingleEvent
 
-abstract class BaseViewModel<A: BaseAction,C: BaseChange,S: BaseState> : BaseViewModel<A, S, C>() {
+abstract class BaseViewModel<A: BaseAction,C: BaseChange,S: BaseState> : RoxieViewModel<A, S, C>() {
 
 
     protected val _navigationLiveData: MutableLiveData<SingleEvent<Route>> = MutableLiveData()
